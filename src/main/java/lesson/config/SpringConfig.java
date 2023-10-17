3807d513eff31710b30577d77e6131d485f6e1aa
WinebearNN <140349456+WinebearNN@users.noreply.github.com>
2023-10-17 08:46:41 +0300

    lesson_18

--- src/main/java/lesson/config/SpringConfig.java
+++ src/main/java/lesson/config/SpringConfig.java
@@ -12,7 +12,7 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
-//gtre//
+
 @Configuration
 @ComponentScan("lesson")
 @EnableWebMvc
---
+++ src/main/java/lesson/controllers/FirstController.java
@@ -0,0 +1,19 @@
+package lesson.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage(){
+        return "goodbye";
+    }
+
+}
